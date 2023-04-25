161039faea46c25e74a71450962d158cb6a9fb5a
Vitor-M <vitorm2000@outlook.com>
2023-04-25 19:48:52 -0300

    feat: ex2 finished

---
+++ logica/ex2.js
@@ -0,0 +1,11 @@
+fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(response => response.json())
+    .then(data => {
+        const randomPhoto = data[Math.floor(Math.random() * data.length)];
+
+        const sameAlbumPhotos = data.filter(photo => photo.albumId === randomPhoto.albumId)
+
+        console.log(sameAlbumPhotos)
+
+    })
+    .catch(error => console.error(error));
