9d583f8702d4b6f612399e0fb6a533e75380e9fb
Vitor-M <vitorm2000@outlook.com>
2023-04-26 07:36:07 -0300

    feat: added default message for undefined funnel step

--- funis-de-compra/ex2.js
+++ funis-de-compra/ex2.js
@@ -2,10 +2,11 @@ const funnels = {
     '/': 'Home',
     '/categoria': 'Categoria',
     '/produto': 'Produto'
-}
+};
 
-let currentPage = window.location.pathname
-let barIndex = currentPage.indexOf('/',1)
-currentPage = barIndex ?  currentPage.substring(0,barIndex) : currentPage
+let currentPage = window.location.pathname;
+const barIndex = currentPage.indexOf('/',1);
+currentPage = barIndex > 0 ?  currentPage.substring(0,barIndex) : currentPage;
 
-console.log(`Etapa do Funil: ${funnels[currentPage]}`)
+let funnelStep =  funnels[currentPage] || 'Não Definida';
+console.log(`Etapa do Funil: ${funnelStep}`);
